72b1acf400be30c9f76608ccc392e6d0b8cd2f46
Carsten Linke <carstenlinke@outlook.com>
2025-01-25 15:01:25 +0100

    Fixed solution item paths.

--- src/NumeriRomani/Exceptions/InvalidRomanNumberException.cs
+++ src/NumeriRomani/Exceptions/InvalidRomanNumberException.cs
@@ -4,14 +4,6 @@
 /// <summary>
 /// The exception that is thrown when a Roman number has an invalid format and cannot be interpreted.
 /// </summary>
-public class InvalidRomanNumberException : Exception
-{
-
-    /// <summary>
-    /// Initializes the <see cref="InvalidRomanNumberException"/>.
-    /// </summary>
-    /// <param name="romanNumber">The invalid Roman number.</param>
-    public InvalidRomanNumberException(string romanNumber) : base($"The string \"{romanNumber}\" cannot be interpreted as a valid Roman number.")
-    { }
-
-}
+/// <param name="romanNumber">The invalid Roman number.</param>
+public class InvalidRomanNumberException(string romanNumber) : Exception($"The string \"{romanNumber}\" cannot be interpreted as a valid Roman number.")
+{ }
